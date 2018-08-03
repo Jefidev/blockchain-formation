968b6dfdf02c6c2d33bd0c89a79bc6402ef3fffd
jefidev <jerome.fink@student.unamur.be>
2018-08-03 21:55:55 +0200

    Representation de la blockchain

---
+++ code/blockchain.py
@@ -0,0 +1,25 @@
+
+
+
+class Blockchain(Object):
+
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creer un nouveau block dans la chaine
+        pass
+
+    def new_transaction(self):
+        # Creer une nouvelle transaction dans la liste
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hash un block
+        pass
+
+    def last_block(self):
+        # Retourne le dernier block de la chaine
+        pass
